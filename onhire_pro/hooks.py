0058ebcad133b6a058d62ab4099571006afce25b
lauchdown <76845652+lauchdown@users.noreply.github.com>
2025-05-22 21:36:40 +0100

    Update hooks.py

--- onhire_pro/hooks.py
+++ onhire_pro/hooks.py
@@ -1,3 +1,7 @@
+app_name = "onhire_pro"
+app_title = "OnHire Pro"
+app_publisher = "OnHire Pro"
+app_description = "Rental Management System"
 app_include_js = [
     "/assets/onhire_pro/js/sales_invoice.js"
 ]
