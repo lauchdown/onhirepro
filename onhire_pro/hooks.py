a44e4c33d2b4b79d2071e76ec91278d1b23283c4
lauchdown <76845652+lauchdown@users.noreply.github.com>
2025-05-22 21:30:21 +0100

    v2

--- onhire_pro/hooks.py
+++ onhire_pro/hooks.py
@@ -1,16 +1,9 @@
 app_include_js = [
-    "/assets/onhire_pro/js/xero_settings.js",
     "/assets/onhire_pro/js/sales_invoice.js"
 ]
 
 doc_events = {
     "Sales Invoice": {
-        "on_submit": "onhire_pro.xero_integration.sync_invoice_to_xero"
     }
 }
 
-scheduler_events = {
-    "hourly": [
-        "onhire_pro.xero_integration.setup_xero_sync_scheduler"
-    ]
-}
